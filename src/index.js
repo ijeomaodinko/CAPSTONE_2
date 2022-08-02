c5eb68841da4aa71711d482bc9b1214347547e9b
vanessaBonito <vanebonito@hotmail.com>
2022-08-02 14:43:15 -0500

    Add header with navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
+
 
  function component() {
    const element = document.createElement('div');
@@ -8,13 +8,6 @@ import Icon from './icon.png';
   // Lodash, now imported by this script
   //  element.innerHTML = _.join(['Hello123', 'webpack'], ' ');
   //  element.classList.add('hello');
-
-   // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
 
    return element;
  }
