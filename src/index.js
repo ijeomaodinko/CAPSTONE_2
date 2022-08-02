9676a301e4fe1cb91b5761179582ef9b69a82141
vanessaBonito <vanebonito@hotmail.com>
2022-08-02 17:18:08 -0500

    Add header styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import _ from 'lodash';
-import './style.css';
+import _ from "lodash";
+import "./style.css";
 
 
- function component() {
-   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  //  element.innerHTML = _.join(['Hello123', 'webpack'], ' ');
-  //  element.classList.add('hello');
 
-   return element;
- }
-
- document.body.appendChild(component());
