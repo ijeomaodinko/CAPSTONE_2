1a4841e8c4eae8768d6902cf806fb16d4a7c5d22
ijeoma <ijeomaodinko@gmail.com>
2022-08-03 11:32:07 -0400

    Add linters files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import _ from 'lodash';
 import './style.css';
-import Icon from './icon.png';
 
  function component() {
    const element = document.createElement('div');
@@ -10,11 +9,7 @@ import Icon from './icon.png';
   //  element.classList.add('hello');
 
    // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
+  
 
    return element;
  }
