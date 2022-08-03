a659b77cd1018eee9e935f6737a9e6bd4afae5f7
vanessaBonito <vanebonito@hotmail.com>
2022-08-03 11:27:33 -0500

    Change colors values

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import "./style.css";
-
-const component = () => {
-  const element = document.createElement("div");
-
-  // Lodash, now imported by this script
-  //  element.innerHTML = _.join(['Hello123', 'webpack'], ' ');
-  //  element.classList.add('hello');
-
-  // Add the image to our existing div.
-
-  return element;
-}
-
-document.body.appendChild(component());
