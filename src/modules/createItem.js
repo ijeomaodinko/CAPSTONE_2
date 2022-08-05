3c19b4218c977facf3ab6f03318f7d3991dc540b
vanessaBonito <vanebonito@hotmail.com>
2022-08-05 10:54:06 -0500

    Fix styles

--- src/modules/createItem.js
+++ src/modules/createItem.js
@@ -1,60 +1,67 @@
 // Import icon
-import like from '../like.png';
-import postLike from './postLike.js';
+import like from "../like.png";
+import postLike from "./postLike.js";
 
 // Function to create a div with show information
 const createItem = (show, likes) => {
   // Create elements
-  const divShow = document.createElement('div');
-  const divImg = document.createElement('div');
-  const imgShow = document.createElement('img');
-  const divShowTitle = document.createElement('div');
-  const h3ShowTitle = document.createElement('h3');
-  const divLikes = document.createElement('div');
-  const numLikes = document.createElement('span');
-  const imgLike = document.createElement('img');
-  const divButtons = document.createElement('div');
-  const btnReserve = document.createElement('button');
-  const btnComments = document.createElement('button');
+  const divShow = document.createElement("div");
+  const divImg = document.createElement("div");
+  const imgShow = document.createElement("img");
+  const divShowTitle = document.createElement("div");
+  const h3ShowTitle = document.createElement("h3");
+  const divLikes = document.createElement("div");
+  const numLikes = document.createElement("span");
+  const imgLike = document.createElement("img");
+  const divButtons = document.createElement("div");
+  const btnReserve = document.createElement("button");
+  const btnComments = document.createElement("button");
 
   // Add content and atributes
-  divShow.classList.add('show');
+  divShow.classList.add("show");
 
   // Show image
-  divImg.classList.add('image_show');
+  divImg.classList.add("image_show");
   imgShow.src = show.image.medium;
   divImg.appendChild(imgShow);
 
   // Show title
-  divShowTitle.classList.add('info_show');
-  h3ShowTitle.classList.add('title_show');
+  divShowTitle.classList.add("info_show");
+  h3ShowTitle.classList.add("title_show");
   h3ShowTitle.textContent = show.name;
   divShowTitle.appendChild(h3ShowTitle);
 
   // Show likes
-  divLikes.classList.add('likes_show');
-  numLikes.classList.add('num_likes');
+  divLikes.classList.add("likes_show");
+  numLikes.classList.add("num_likes");
   const likesItem = likes.find((element) => element.item_id === show.id).likes;
   numLikes.textContent = likesItem;
-  imgLike.classList.add('img_like');
+  imgLike.classList.add("img_like");
   imgLike.src = like;
   divLikes.appendChild(numLikes);
   divLikes.appendChild(imgLike);
 
   // Show buttons
-  divButtons.classList.add('buttons_show');
-  btnComments.textContent = 'Comments';
-  btnComments.classList.add('btn_show');
-  btnReserve.textContent = 'Reserve';
-  btnReserve.classList.add('btn_show');
+  divButtons.classList.add("buttons_show");
+  btnComments.textContent = "Comments";
+  btnComments.classList.add("btn_show");
+  btnReserve.textContent = "Reserve";
+  btnReserve.classList.add("btn_show");
   divButtons.appendChild(btnComments);
   divButtons.appendChild(btnReserve);
 
   /// ADD EVENTS LISTENERS
-  imgLike.addEventListener('click', async () => {
+
+  //Add new likes
+  imgLike.addEventListener("click", async () => {
     postLike(show.id);
     const likes = parseInt(numLikes.textContent, 10) + 1;
     numLikes.textContent = likes;
+  });
+
+  //Show modal
+  btnComments.addEventListener("click", async () => {
+    
   });
 
   // Add child
